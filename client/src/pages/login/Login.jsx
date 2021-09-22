7e8333bb70b34556cc3f7805722e51cbbcca0473
sgragg5619 <sgragg5619@gmail.com>
2021-09-22 19:06:09 +0800

    Branding Change

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
 import { CircularProgress } from "@material-ui/core";
+import appName from "../../branding";
 
 export default function Login() {
   const email = useRef();
@@ -21,9 +22,9 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
+          <h3 className="loginLogo">{appName}</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Connect with friends and the world around you on {appName}.
           </span>
         </div>
         <div className="loginRight">
